0dd571a7d09ef11c7b407c41c827c1f15d6aa9ef
PhilippBR0 <68929021+PhilippBR0@users.noreply.github.com>
2023-10-02 15:04:20 -0300

    Update HorasTrabalhadas.cpp

--- HorasTrabalhadas.cpp
+++ HorasTrabalhadas.cpp
@@ -2,11 +2,12 @@
 
 #include <iostream>
 #include <iomanip>
+#include <limits> // Para usar numeric_limits
 
 using namespace std;
 
 int main() {
-    double salarioMensal = 1200.0; // Salário mensal fixo em R$ 1200
+    double salarioMensal = 1200.0;
     double valorHora = salarioMensal / (20 * 8); // Valor da hora considerando um mês com 20 dias úteis e 8 horas por dia
     double valorHoraExtra = valorHora * 1.5; // Valor da hora extra (50% a mais)
 
@@ -14,17 +15,32 @@ int main() {
     int diasTrabalhados;
     double horasExtrasMes;
 
+    // Função para verificar se a entrada é um número válido
+    auto getDoubleInput = [](const string& prompt) -> double {
+        double input;
+        while (true) {
+            cout << prompt;
+            if (cin >> input) {
+                // A entrada é um número válido
+                break;
+            } else {
+                // Limpar o estado de erro de cin e descartar o valor não numérico
+                cin.clear();
+                cin.ignore(numeric_limits<streamsize>::max(), '\n');
+                cout << "Entrada inválida. Por favor, digite um número válido." << endl;
+            }
+        }
+        return input;
+    };
+
     // Solicitar o número de horas trabalhadas em um dia
-    cout << "Digite o número de horas trabalhadas em um dia: ";
-    cin >> horasTrabalhadasDia;
+    horasTrabalhadasDia = getDoubleInput("Digite o número de horas trabalhadas em um dia: ");
 
     // Solicitar o número de dias trabalhados no mês
-    cout << "Digite o número de dias trabalhados no mês: ";
-    cin >> diasTrabalhados;
+    diasTrabalhados = getDoubleInput("Digite o número de dias trabalhados no mês: ");
 
     // Solicitar o número de horas extras trabalhadas no mês
-    cout << "Digite o número de horas extras trabalhadas no mês: ";
-    cin >> horasExtrasMes;
+    horasExtrasMes = getDoubleInput("Digite o número de horas extras trabalhadas no mês: ");
 
     // Calcular as horas trabalhadas no dia, no mês e no ano
     double horasTrabalhadasMes = horasTrabalhadasDia * diasTrabalhados;
